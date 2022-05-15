ca93ea9bebbfcd21ccf0271b9fb7c71673774596
vlad <bbladik12@gmail.com>
2022-05-15 23:33:13 +0300

    some fixes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
 import TelegramLoginButton from "telegram-login-button";
+import {useNavigate} from "react-router-dom";
 
 const Login = ({setUserData}) => {
+    const navigate = useNavigate()
 
     const onAuth = (ctx) => {
-        console.log(ctx)
+        setUserData(ctx)
+        navigate('..', {replace: true})
     }
 
     return (
         <div>
             <h2>For login throw through telegram push the button here </h2>
-            <TelegramLoginButton botName={'reacttgauth_bot'} dataOnauth={(user) => console.log(user)}/>
+            <TelegramLoginButton botName={'reacttgauth_bot'} dataOnauth={onAuth}/>
         </div>
     );
 };
